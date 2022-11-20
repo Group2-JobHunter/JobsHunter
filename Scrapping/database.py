e969eb59c30c79dd8d504ced007bb49de58b98c5
Your Nameibrahimnalm <ibrahimnalmanaseer@gmail.com>
2022-11-20 15:39:09 +0300

    'done'

--- Scrapping/database.py
+++ Scrapping/database.py
@@ -7,22 +7,20 @@ class Database():
   
         
 
-    db = sqlite3.connect('app.db')
+    db = sqlite3.connect('app.db',check_same_thread=False)
     cr = db.cursor()
     
-    cr.execute(
-            'create table if not exists Jobs (Source text,Title text, Company text, Date text,City text,Country text,Percatnage text,Link text )')
+    # cr.execute(
+    #         'create table if not exists Jobs (Source text,Title text, Company text, Date text,City text,Country text,Percatnage text,Link text )')
     
     def save_data(self,data):
-        # Database.cr.execute(
-        #     'create table if not exists Jobs (Source text,Title text, Company text, Date text,City text,Country text,Percatnage text,Link text )')
+        Database.cr.execute(
+            'create table if not exists Jobs (Source text,Title text, Company text, Date text,City text,Country text,Percatnage text,Link text )')
 
         
         Database.cr.executemany("INSERT INTO Jobs VALUES (?,?,?,?,?,?,?,?)",data)
-
         Database.db.commit()
-
-        Database.db.close()
+        # Database.db.close()
 
     def fetch_data(self):
       
