e750068e93fa000907c1dc65bb4d823fb1f9a7a0
Your Nameibrahimnalm <ibrahimnalmanaseer@gmail.com>
2022-11-21 14:34:07 +0300

    'done'

--- Scrapping/WebScraper.py
+++ Scrapping/WebScraper.py
@@ -9,7 +9,7 @@ class WebScraper(ABC):
     def __init__(self,headless):
         options = Options()
         user_agent = (
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+                    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
         )
         options.headless = headless
         options.add_experimental_option("detach", True)
